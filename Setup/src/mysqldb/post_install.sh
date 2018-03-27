0d4d260a45fd8a9489a9ef6390b378d17f0388d5
judyfong <judyyfong@gmail.com>
2018-03-27 12:57:33 +0000

    Update mysqldb script to not be interactive

--- Setup/src/mysqldb/post_install.sh
+++ Setup/src/mysqldb/post_install.sh
@@ -17,19 +17,21 @@
 # File author/s:
 #     Simon Klüpfel <simon.kluepfel@gmail.com>
 #     Matthias Petursson <oldschool01123@gmail.com>
+# 	  Judy Fong <judyfong@ru.is>
 
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] || {
   echo "$0: This script should be sourced, not executed!"
   exit 1
 }
 
-echo "Are you sure you want to run --mysqldb, it will delete the entire mysql database? Note you can run './setup.sh --all --no-mysqldb' to avoid this. (type 1 or 2)"
-select yn in "Yes" "No"; do
-    case $yn in
-        # At this point we are working in Local/
-        Yes ) ../Backend/db/erase_and_rewind.sh; break;;
-        No ) break;;
-    esac
-done
+#Comment out this section to make it less interactive
+#echo "Are you sure you want to run --mysqldb, it will delete the entire mysql database? Note you can run './setup.sh --all --no-mysqldb' to avoid this. (type 1 or 2)"
+#select yn in "Yes" "No"; do
+#    case $yn in
+#        # At this point we are working in Local/
+#        Yes ) ../Backend/db/erase_and_rewind.sh; break;;
+#        No ) break;;
+#    esac
+#done
 
 return 0
